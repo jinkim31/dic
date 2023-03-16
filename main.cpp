e74641dbd9e6d968b16e8b361805050e3b4f566e
jinkim31 <jinkim@kaist.ac.kr>
2023-03-16 14:47:45 +0900

    added eigen, nanoflann

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include <Eigen/Eigen>
 
 int main()
 {
-    cv::Mat mat = cv::Mat(1000, 1000, CV_8UC1);
-
-    std::cout<<mat.size<<std::endl;
-    cv::imshow("a", mat);
-    cv::waitKey();
+    Eigen::Matrix2f m;
+    m <<1, 2,1, 2;
+    std::cout<<m;
 }
